ee508ed30f2e8ff7774af1350d9a28bd3b591370
lunguyenhe <105269930+lunguyenhe@users.noreply.github.com>
2022-05-31 10:55:16 +0700

    Update Account.java

--- src/conf/java/Entity/Account.java
+++ src/conf/java/Entity/Account.java
@@ -10,5 +10,46 @@ package Entity;
  * @author Admin
  */
 public class Account {
+    String username;
+    String password;
+    int role;
+
+    public Account() {
+    }
+
+    public Account(String username, String password, int role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" + "username=" + username + ", password=" + password + ", role=" + role + '}';
+    }
     
 }
